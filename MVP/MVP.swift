b0a22825f561246ac9c185ccf38d6268800fa67f
Ethan Perkins <ethanperkinspro@gmail.com>
2021-12-09 15:01:52 -0700

    updated

--- MVP/MVP.swift
+++ MVP/MVP.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+//  Pitch/Idea: A mobile application that allows users to sync contacts, and get reminded about birthdays, annaversaries, etc.
+
+// users can make an account
+// users can sync their contacts
+// users can set when to be reminded about an upcoming event
+// users can choose to call/text the person of interest
+// users can pre generate a text to send the second the date occurs
+// users can set all dates in advance
+
+// Future Features:
+// users can interact with the iMessage api
+// users can call recipients from the app itself
+// users can import followers/friends from other social media websites
